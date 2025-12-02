5b594280eb021b14912920bc2b8200f685e665ac
ranggo pato <patoranggo@gmail.com>
2025-12-03 01:05:51 +0700

    sk1: styling about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,41 @@
 <template>
-    <h1>About Page</h1>
+    <div class="about-container">
+        <h1>Tentang Aplikasi</h1>
+        <div class="card">
+            <p>Task Manager sederhana, untuk mengelola daftar tugas. Aplikasi ini dibuat untuk study kasus pembelajaran Vue.js dengan materi :</p>
+            <ul>
+                <li>Reactive State</li>
+                <li>Attribute binding dan Interpolation</li>
+                <li>Watch dan Computed Property</li>
+                <li>Props, emit, slot</li>
+                <li>Composables</li>
+                <li>Lifecyle hooks</li>
+                <li>Vue router, dll</li>
+            </ul>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.about-container {
+  max-width: 600px;
+  margin: 30px auto;
+}
+
+h1 {
+  margin-bottom: 15px;
+}
+
+.card {
+  background: white;
+  border: 1px solid #e5e7eb;
+  padding: 16px;
+  border-radius: 8px;
+  line-height: 1.7;
+}
+
+.card ul {
+  padding-left: 20px;
+  margin-top: 10px;
+}
+</style>
